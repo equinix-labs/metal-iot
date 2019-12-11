5fb896bfbf549adea595f377d5aa3f97b8ea4473
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2019-12-11 16:15:42 +0000

    Perform insert into new schema
    * Add new schema
    * Insert new rows with correct JSON example as per README
    * Give debugging instructions for OpenFaaS
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- openfaas/services/db-inserter/handler.js
+++ openfaas/services/db-inserter/handler.js
@@ -1,42 +1,50 @@
 "use strict"
+
 const { Client } = require('pg')
 const Pool = require('pg').Pool
 const fs = require('fs')
-var pool;
 
-module.exports = async (event, context) => {
-    if(!pool) {
-        const poolConf = {
-            user: fs.readFileSync("/var/openfaas/secrets/db-username", "utf-8"),
-            host: fs.readFileSync("/var/openfaas/secrets/db-host", "utf-8"),
-            database: process.env["db_name"],
-            password: fs.readFileSync("/var/openfaas/secrets/db-password", "utf-8"),
-            port: process.env["db_port"],
-        };
-        // console.log(poolConf);
+const pool = initPool()
 
-        pool = new Pool(poolConf)
+module.exports = async (event, context) => {  
+    let client = await pool.connect()
+
+    if(event.method == "POST") {
+        let {name, location, tempCelsius, batteryMv} = event.body;
+        console.log(name, location, tempCelsius, batteryMv)
+        await insert(client, name, location, tempCelsius, batteryMv);
+
+        client.release()
+        return context.status(200).succeed({"status": "OK"});
     }
-
-    await pool.connect()
-
-    try {
-        const res = await pool.query(`CREATE TABLE drone_position (
-        positionid        bigint primary key,
-        name              text,
-        location          point,
-        temp_celsius      double precision not null,
-        created           timestamp with time zone default now()
-    );`)
-        console.log(res)
-    } catch(e) {
-        console.error(e)
-    }
-
-    const insertRes = await pool.query(`insert into drone_position (positionid, name, location, temp_celsius) values (1, 'Drone A', point(1,52), 8.5);`)   
-    console.log(insertRes)
-
-    await pool.end()
-
-    context.status(200).succeed("OK");
+    
+    client.release()
+    return context.status(200).succeed({"status": "No action"});
 }
+
+async function insert(client, name, location, tempCelsius, batteryMv) {
+    let res = await client.query(`insert into drone_position (name, location, temp_celsius, battery_mv) values ($1, $2, $3, $4);`,
+    [name, new Point(location).toString(), tempCelsius, batteryMv]);
+    console.log(res);
+}
+
+function initPool() {
+  return new Pool({
+    user: fs.readFileSync("/var/openfaas/secrets/db-username", "utf-8"),
+    host: fs.readFileSync("/var/openfaas/secrets/db-host", "utf-8"),
+    database: process.env["db_name"],
+    password: fs.readFileSync("/var/openfaas/secrets/db-password", "utf-8"),
+    port: process.env["db_port"],
+  });
+ }
+
+ class Point {
+     constructor(point) {
+         this.lon = point.lon
+         this.lat = point.lat
+     }
+
+     toString() {
+         return "(" + this.lon+"," + this.lat+")"
+     }
+ }
