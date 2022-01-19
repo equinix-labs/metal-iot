722b16c7ea021f31aacbae07dc0130735e888539
Marques Johansson <mjohansson@equinix.com>
2022-01-19 08:01:20 -0500

    update k8s/ for newer Terraform standards
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- k8s/main.tf
+++ k8s/main.tf
@@ -1,3 +1,37 @@
+provider "metal" {
+  auth_token = var.auth_token
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "priv_key" {
+  content         = chomp(tls_private_key.ssh_key.private_key_pem)
+  filename        = "ssh_priv_key"
+  file_permission = "0600"
+}
+
+data "template_file" "user_data" {
+  template = file("templates/user_data.sh")
+  vars = {
+    priv_key = chomp(tls_private_key.ssh_key.private_key_pem)
+    pub_key  = chomp(tls_private_key.ssh_key.public_key_openssh)
+  }
+}
+
+resource "metal_device" "k3s_nodes" {
+  count            = var.node_count
+  hostname         = format("%s-%02d", var.hostname, count.index + 1)
+  plan             = var.node_size
+  facilities       = [var.facility]
+  operating_system = var.operating_system
+  billing_cycle    = var.billing_cycle
+  project_id       = var.project_id
+  user_data        = data.template_file.user_data.rendered
+}
+
 data "template_file" "k3s_install_script" {
   count    = length(metal_device.k3s_nodes)
   template = file("templates/k3s_install.sh")
